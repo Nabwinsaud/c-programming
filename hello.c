d64fabb86811b88347f262e5d5dba4de709816e8
Nabwinsaud <codinglogic1@gmail.com>
2022-04-01 20:17:44 +0545

    fundamental of c programming

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+// stdio.h is a header file used for i/o input output ie prinf and scanf
+int main(){
+// main content goes here ....
+
+
+printf("Hello world \n");
+int a=2;
+printf("%d",a);
+    return 0;
+}
+
+
+
+
---
+++ tuto1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// header file
+// std- standard io-input output 
+
+
+// (parenthesis)
+
+int  main(){
+
+  printf("Hello world");
+  printf("Nice to see you on our channel ");
+  printf("subscribe to our channel");
+
+    return 0;
+}
+
+
+
+
